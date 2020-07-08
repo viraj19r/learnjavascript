6b7c5408044595ba1ed9ddc3d770d9e9543f6795
rahul.name <rahul@mail.com>
2020-07-08 21:55:14 +0530

    first program

--- practice.js
+++ practice.js
@@ -1,1 +1,17 @@
-alert("hello");
+let var1;    // variable declared
+var1 = 1;   //variable initialized
+ let var2 = 2;  //variable declared and initialized
+
+ const constVar = 2;   //constant variable
+  const var6 = (2+3*3/1)+5;  //16
+
+  let var3 = "a string" + 'a string' + `a string`;
+  let var4 = `${var1} 10 string`;  //template literal
+  let var5 = var1 + "10 string";
+
+    function add(num1, num2){      //create a function
+        const result = num1 + num2;
+        console.log(result);
+    }
+
+   add(4,7);   // 11 call function
