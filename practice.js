f88f296cf3ecb42e48ca8f8fa76653848ae03b61
rahul.name <rahul@mail.com>
2020-07-08 23:35:28 +0530

    todays changes

--- practice.js
+++ practice.js
@@ -1,17 +1,40 @@
-let var1;    // variable declared
-var1 = 1;   //variable initialized
- let var2 = 2;  //variable declared and initialized
+let var1; // variable declared--- global scope
+var1 = 1; //variable initialized
+let var2 = 2; //variable declared and initialized
 
- const constVar = 2;   //constant variable
-  const var6 = (2+3*3/1)+5;  //16
+const constVar = 2; //constant variable
+const var6 = 2 + (3 * 3) / 1 + 5; //16
 
-  let var3 = "a string" + 'a string' + `a string`;
-  let var4 = `${var1} 10 string`;  //template literal
-  let var5 = var1 + "10 string";
+let var3 = "a string" + "a string" + `a string`;
+let var4 = `${var1} 10 string`; //template literal
+let var5 = var1 + "10 string";
+let var7 = "string should be " +
+               "concatenated" +
+               "in this way";
 
-    function add(num1, num2){      //create a function
-        const result = num1 + num2;
-        console.log(result);
-    }
 
-   add(4,7);   // 11 call function
+//Functions:
+function add(num1, num2) {
+  //create a function
+  const result = num1 + num2; // result -- local or block scope
+  return result;
+}
+
+console.log(add(3,5)); // 8 call function
+
+
+function first(){
+    alert("first function");
+}
+
+function second(name){
+    alert(name);
+}
+first();
+second("second function");
+
+
+function third(string1,string2,string3){
+    return string1 + string2 + string3;
+}
+alert(third("one","two","three"));
