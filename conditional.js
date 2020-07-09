11375b0ab111179d2dadccaeb6fc9abe294a0f77
rahul.name <rahul@mail.com>
2020-07-09 17:24:39 +0530

    add arra

--- conditional.js
+++ conditional.js
@@ -1,3 +1,15 @@
-console.log("a">"b");
-console.log("ab">"aa");
-console.log("a">"A");
+if ("a" > "b") {
+  console.log("a is greater than b");
+} else {
+  console.log("b is greater than a");
+}
+if ("ab" > "aa") {
+  console.log("ab is greater than aa");
+} else {
+  console.log("aa is greater than ab");
+}
+if ("a" > "A") {
+  console.log("a is greater than A");
+} else {
+  console.log("A is greater than a");
+}
