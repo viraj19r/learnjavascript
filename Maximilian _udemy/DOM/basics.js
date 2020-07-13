979dba788ba58064642ee8b6f7769fef235b82d8
viraj sharma <viraj19r@gmail.com>
2020-07-13 16:05:28 +0530

    dom basics

--- Maximilian _udemy/DOM/basics.js
+++ Maximilian _udemy/DOM/basics.js
@@ -11,4 +11,21 @@
 const para2 = document.getElementsByTagName("p");
 para2[0].style.color = "red";
 
-para2[0].textContent = "welcome";
+para2[0].textContent = "Welcome";
+para2[0].textContent = "Your " + para2[0].textContent;
+
+// child traversing
+const uList = document.querySelector("ul");
+uList.children[0].style.color = "yellow";      // children only selects elements
+// uList.firstChild.style.color = "yellow";     // same as above
+uList.childNodes[3].style.color = "blue";   // here childNodes also selects text nodes, array formed is [text,li,text,li,text,li]
+
+// parent traversing
+const var1 = document.querySelector("li");
+ var1.parentElement.style.backgroundColor = "green"; // parent(ul) now has background color green
+
+ // to get another ancestor after parent we use
+ var1.closest("body").style.backgroundColor = "grey";
+
+ // selecting sibling element
+ uList.previousElementSibling.style.backgroundColor = "yellow";
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# learnjavascript
+# learnJavascript
 
 ## * [course link](https://www.udemy.com/course/javascript-the-complete-guide-2020-beginner-advanced/)
 
