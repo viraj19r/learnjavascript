9d1ff04b47c091a3e59aef79ee6187745c35899a
viraj sharma <viraj19r@gmail.com>
2020-07-13 13:03:07 +0530

    add dom

---
+++ Maximilian _udemy/DOM/basics.js
@@ -0,0 +1,14 @@
+// const para = document.getElementById('main_p');
+
+// para.textContent = 'hello world';
+// para.style.color =  "yellow";
+// para.style.backgroundColor = "black";
+
+// const para1 = document.querySelector("p");
+
+// para1.style.color = "blue";
+
+const para2 = document.getElementsByTagName("p");
+para2[0].style.color = "red";
+
+para2[0].textContent = "welcome";
