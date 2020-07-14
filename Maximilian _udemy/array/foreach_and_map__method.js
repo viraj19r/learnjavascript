29bea29038f8e69442c98139f7b9346f1ae4f5d4
viraj sharma <viraj19r@gmail.com>
2020-07-14 21:55:10 +0530

    add new folder

--- Maximilian _udemy/array/foreach_and_map__method.js
+++ Maximilian _udemy/array/foreach_and_map__method.js
@@ -4,11 +4,11 @@ const taxAmount = [];
 // for (const elements of amount){
 //     taxAmount.push(elements*(1+tax));
 // }
-// amount.forEach((taxAm,index,wholeArray)=>{ // forEach(all values of array,index of array, whole array)
+// amount.forEach((taxAm,index,wholeArray)=>{ // forEach(all values of array,index of array, whole array) [it points to the same memory so we cannot assign it to a new variable]
 //    taxAmount.push(taxAm*(1+tax));  
 // });
 console.log(taxAmount);
 const addTaxToAmount = amount.map((taxAm,index,wholeArray)=>{ // map will now return(it must return something) a new array pointing to a new memory. so we can store in a variable
   return  taxAmount.push(taxAm*(1+tax));  
  });
- console.log(taxAmount);
+ console.log(taxAmount);// 
