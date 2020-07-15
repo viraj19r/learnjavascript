e95396d71471506c937b9dd1abdd76ff257962da
viraj sharma <viraj19r@gmail.com>
2020-07-15 11:13:18 +0530

    prettier

--- Maximilian _udemy/array/foreach_and_map__method.js
+++ Maximilian _udemy/array/foreach_and_map__method.js
@@ -5,10 +5,11 @@ const taxAmount = [];
 //     taxAmount.push(elements*(1+tax));
 // }
 // amount.forEach((taxAm,index,wholeArray)=>{ // forEach(all values of array,index of array, whole array) [it points to the same memory so we cannot assign it to a new variable]
-//    taxAmount.push(taxAm*(1+tax));  
+//    taxAmount.push(taxAm*(1+tax));
 // });
 console.log(taxAmount);
-const addTaxToAmount = amount.map((taxAm,index,wholeArray)=>{ // map will now return(it must return something) a new array pointing to a new memory. so we can store in a variable
-  return  taxAmount.push(taxAm*(1+tax));  
- });
- console.log(taxAmount);// 
+const addTaxToAmount = amount.map((taxAm, index, wholeArray) => {
+  // map will now return(it must return something) a new array pointing to a new memory. so we can store in a variable
+  return taxAm * (1 + tax);
+});
+console.log(addTaxToAmount); //
--- Maximilian _udemy/objects/spread_operator_assign_method.js
+++ Maximilian _udemy/objects/spread_operator_assign_method.js
@@ -1,31 +1,29 @@
 const person = {
-    "first name": "Dan",  
-    age: "30",
-    1.5 : "hello",
-    hobbies: ["playing", "coding"], 
-    greet: () => "hi there",
-  };
-//   const person2 = {...person};// objects are reference values so we cannot assign them to new variable(they will point to same memory), that is why we use spread operator to copy all object key values to another constant pointing towards new memory location
-  const person2 = {...person, age:"29"} // this will change age
-  const per = {country : "india", planet: "earth",galaxy: "milky way"};
-  // assign method to copy -- assign(target(where to copy),source1,source2,....)
-  
-  const person3 = Object.assign({},person,person2,per); //all changes will be overridden if there any in previous key value pair and output will be consoled(now only one age 29 will be shown in output)
-  
-  
-  
-  // object destructuring 
-  const {planet,...others} = per;
-  const {planet:ourPlanet} = per;// assign new name
-  
-  // console.log(ourPlanet);    
+  "first name": "Dan",
+  age: "30",
+  1.5: "hello",
+  hobbies: ["playing", "coding"],
+  greet: () => "hi there",
+};
+// const person2 = {...person};// objects are reference values so we cannot assign them to new variable(they will point to same memory), that is why we use spread operator to copy all object key values to another constant pointing towards new memory location
+const person2 = { ...person, age: "29" }; // this will change age
+const per = { country: "india", planet: "earth", galaxy: "milky way" };
+// assign method to copy -- assign(target(where to copy),source1,source2,....)
 
-  // checking for property existence
-  if("planet" in per){
-    console.log("yes");
-  }
-  if(!("country" in per)){
-    console.log("no");
-  }else {
-    console.log("yes");
-  }
+const person3 = Object.assign({}, person, person2, per); //all changes will be overridden if there any in previous key value pair and output will be consoled(now only one age 29 will be shown in output)
+
+// object destructuring
+const { planet, ...others } = per;
+const { planet: ourPlanet } = per; // assign new name
+
+// console.log(ourPlanet);
+
+// checking for property existence
+if ("planet" in per) {
+  console.log("yes");
+}
+if (!("country" in per)) {
+  console.log("no");
+} else {
+  console.log("yes");
+}
