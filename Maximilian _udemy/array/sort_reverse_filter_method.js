c1a7edcac97e907ea6175e923fb330a464932766
viraj sharma <viraj19r@gmail.com>
2020-07-15 11:53:50 +0530

    use ternary operator instead of if else

--- Maximilian _udemy/array/sort_reverse_filter_method.js
+++ Maximilian _udemy/array/sort_reverse_filter_method.js
@@ -1,12 +1,14 @@
 const amount = [10.7, 4, 5.6, 3, 6, 2.66];
 amount.sort((a, b) => {
-  if (a > b) {
-    return 1;
-  } else if (a === b) {
-    return 0;
-  } else {
-    return -1;
-  }
+  // if (a > b) {
+  //   return 1;
+  // } else if (a === b) {
+  //   return 0;
+  // } else {
+  //   return -1;
+  // }
+  // we can use ternary operator
+  return a > b ? 1 : a === b ? 0 : -1;
 }); // it converts to string and then sort that is why 10.7 comes first in o/p because 1 is smallest.
 
 console.log(amount);
