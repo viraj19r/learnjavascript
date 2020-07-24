8b2fd21bac812557706acf03e0b0cd1105ca7d3f
viraj sharma <viraj19r@gmail.com>
2020-07-24 12:06:23 +0530

    functional paradigms

--- Maximilian _udemy/programming_paradigms/procedural.js
+++ Maximilian _udemy/programming_paradigms/procedural.js
@@ -1,30 +1,30 @@
-const form = document.getElementById('user-input');
+const form = document.getElementById("user-input");
 
 function signupHandler(event) {
   event.preventDefault();
 
-  const userNameInput = document.getElementById('username');
+  const userNameInput = document.getElementById("username");
   const enteredUsername = userNameInput.value;
 
-  const passwordInput = document.getElementById('password');
+  const passwordInput = document.getElementById("password");
   const enteredPassword = passwordInput.value;
 
   if (enteredUsername.trim().length === 0) {
-    alert('Invalid input - username must not be empty!');
+    alert("Invalid input - username must not be empty!");
     return;
   }
   if (enteredPassword.trim().length <= 5) {
-    alert('Invalid input - password must be six characters or longer.');
+    alert("Invalid input - password must be six characters or longer.");
     return;
   }
 
   const user = {
     userName: enteredUsername,
-    password: enteredPassword
+    password: enteredPassword,
   };
 
   console.log(user);
-  console.log('Hi, I am ' + user.userName);
+  console.log("Hi, I am " + user.userName);
 }
 
-form.addEventListener('submit', signupHandler);
+form.addEventListener("submit", signupHandler);
